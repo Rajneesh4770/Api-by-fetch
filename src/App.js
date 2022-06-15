684261ef6d05e479eccaa990d668cb3f130ccc13
Rajneesh <rajneesh4770@gmail.com>
2022-06-15 12:57:32 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-// import logo from './logo.svg';
-import './App.css';
-// import React from 'react';
-import React, { useEffect, useState} from 'react';
-import Api from './component/Api';
-import Test from './component/Test';
-// import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import Api from "./component/Api";
 
-// class App extends React.Component{
-//   render()
-function App(){
-return(
-  <div>
-    <Api/>
-    <Test />
-    {/* <Router>
-      
-      <Link to=" ">Api</Link>
-      <Link to="/Test">Test</Link>
-
-      <Route  path="./component/Api" Component={Api}/>
-      <Route path ="./component/Test" Component={Test} />
-     
-    </Router> */}
+function App() {
+  return (
+    <div>
+      <Api />
     </div>
- 
-  
-  
-  
-)
+  );
 }
 
-
-
-
-
-export default App
-
-
-
+export default App;
--- src/component/Api.js
+++ src/component/Api.js
@@ -1,37 +1,36 @@
-import React,{useEffect,useState} from "react";
-function Api(){
-
-const[data,setData]=useState([ ])
-useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/ablums/1/photos").then((result)=>{
-        result.json().then((resp)=>{
-            setData(resp)
-        })
-    })
-},[])
-console.warn(data)
-return(
+import React, { useEffect, useState } from "react";
+function Api() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/ablums/1/photos").then(
+      (result) => {
+        result.json().then((resp) => {
+          setData(resp);
+        });
+      }
+    );
+  }, []);
+  console.warn(data);
+  return (
     <div className="Api">
-    <h1>Get API</h1>
-    <table border="1">
-        <tr >
-            <td>AlbumId</td>
-            <td>Id</td>
-            <td>Url</td>
-            <td>ThumbnailUrl</td>
+      <h1>Get API</h1>
+      <table border="1">
+        <tr>
+          <td>AlbumId</td>
+          <td>Id</td>
+          <td>Url</td>
+          <td>ThumbnailUrl</td>
         </tr>
-        {
-            data.map((item)=>
-            <tr>
+        {data.map((item) => (
+          <tr>
             <td>{item.albumId}</td>
             <td>{item.id}</td>
             <td>{item.url}</td>
             <td>{item.thumbnailUrl}</td>
-            </tr>
-            )
-        }
-    </table>
+          </tr>
+        ))}
+      </table>
     </div>
-)
+  );
 }
 export default Api;
